33c56895140f1cda866f01e140945fcdf61e5fe9
Gnagno <gnagno@gnagno.it>
2011-07-14 15:41:55 +0200

    first commit

---
+++ lib/lost-i18n-translation.rb
@@ -0,0 +1,2 @@
+module LostI18nTranslation
+end
---
+++ spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = 'f03106ed81610cde6294919d37d676b8ed38628aa3f68e76233b7b5d05817a205dc3f6e3bd6e36b659ca4bf56a2e323ba887fc9dd587b74793534a9ef2f8772c'
